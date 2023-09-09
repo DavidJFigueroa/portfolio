1a1746383bfdf808a094afe39702d6f9049fd552
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-09-10 01:50:46 +0200

    feat. Contact form added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 (function () {
-  let form = document.querySelector('#contact-form');
-  let emailInput = document.querySelector('#contact-email');
-  let telephoneInput = document.querySelector('#contact-tel');
+  let form = document.querySelector("#contact-form");
+  let emailInput = document.querySelector("#contact-email");
+  let telephoneInput = document.querySelector("#contact-tel");
 
-  function validateEmail () {
+  function validateEmail() {
     let value = emailInput.value;
-    let hasAtSign = value.indexOf('@') > -1;
-    let hasDot = value.indexOf('.') > -1;
+    let hasAtSign = value.indexOf("@") > -1;
+    let hasDot = value.indexOf(".") > -1;
     return value && hasAtSign && hasDot;
   }
 
-  function validateTelephone () {
+  function validateTelephone() {
     let value = telephoneInput.value;
-    let regEx = /^\+{0,2}([\-\. ])?(\(?\d{0,3}\))?([\-\. ])?\(?\d{0,3}\)?([\-\. ])?\d{3}([\-\. ])?\d{4}/;
+    let regEx =
+      /^\+{0,2}([\-\. ])?(\(?\d{0,3}\))?([\-\. ])?\(?\d{0,3}\)?([\-\. ])?\d{3}([\-\. ])?\d{4}/;
     if (value.match(regEx)) {
       return true;
     } else {
       alert("Please enter a valid phone number!");
-      return false; 
+      return false;
     }
   }
 
@@ -25,10 +26,10 @@
     return validateEmail() && validateTelephone();
   }
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault(); //Do not submit to server
     if (validateForm()) {
-      alert('Success!');
+      alert("Success!");
     }
-  })
+  });
 })();
